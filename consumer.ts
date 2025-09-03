ac7360e9f286c046dc258a0ecc1e4ef85a78a6c1
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-09-04 00:41:10 +0500

    kafka done

--- consumer.ts
+++ consumer.ts
@@ -1,8 +1,9 @@
 import { kafka } from "./client"
+const group = process.argv[2] || "default-group";
 
 async function init() {
   const consumer = kafka.consumer({
-    groupId: "user-1"
+    groupId: group
   })
 
   await consumer.connect()
@@ -12,7 +13,7 @@ async function init() {
   await consumer.run({
     eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
       const valueStr = message.value ? message.value.toString() : "";
-      console.log(`[${topic}]: PART:${partition}: ${valueStr}`)
+      console.log(`${group} : [${topic}]: PART:${partition}: ${valueStr}`)
     }
   })
 
--- producer.ts
+++ producer.ts
@@ -1,22 +1,39 @@
 import { kafka } from "./client";
+import readline from "readline"
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 async function init() {
   const producer = kafka.producer()
   await producer.connect()
   console.log("Connected Successfully Producer!")
 
-  await producer.send({
-    topic: "rider-updates",
-    messages: [
-      {
-        partition: 0, // bad mai solve karein ge
-        key: "location-updates",
-        value: JSON.stringify({ name: "Tony Stark", loc: "Hafizabad" })
-      }
-    ]
+  // Getting prompt from user
+  rl.setPrompt("> ") // To show user to input
+  rl.prompt() // To get value from user
+
+  // When user will input the value this event will trigger
+  rl.on("line", async (line) => {
+    const [riderName, location] = line.split(" ")
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location?.toLowerCase() === "punjab" ? 0 : 1,
+          key: "location-updates",
+          value: JSON.stringify({ name: riderName, location })
+        }
+      ]
+    })
+  }).on("close", async () => {
+
+    await producer.disconnect()
+    console.log("Producer disconnected!")
   })
 
-  await producer.disconnect()
-  console.log("Producer disconnected!")
+
 }
 init()
