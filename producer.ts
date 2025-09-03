68bc5093a9e6068276c2285a782f18f070cd06a4
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-09-04 00:02:58 +0500

    producer done

--- producer.ts
+++ producer.ts
@@ -5,6 +5,18 @@ async function init() {
   await producer.connect()
   console.log("Connected Successfully Producer!")
 
+  await producer.send({
+    topic: "rider-updates",
+    messages: [
+      {
+        partition: 0, // bad mai solve karein ge
+        key: "location-updates",
+        value: JSON.stringify({ name: "Tony Stark", loc: "Hafizabad" })
+      }
+    ]
+  })
 
+  await producer.disconnect()
+  console.log("Producer disconnected!")
 }
 init()
